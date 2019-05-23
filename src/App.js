e8364546a6c1da13049181c3604f4f1726c09c85
justgarcia5 <justgarcia5@gmail.comgit config --global user.email justgarcia5@gmail.com>
2019-05-22 18:49:40 -0700

    made changes to backgroung

--- src/App.js
+++ src/App.js
@@ -16,32 +16,30 @@ class App extends Component {
     return (
       <div>
         <div>
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossOrigin="anonymous"></link>
-
-
-        <Layout >
-          <Header  className="header-links" title={<Link className="justin-header-link"  to="/">Justin Garcia</Link>} scroll>
-            <Navigation >
-              <Link to="/resume">Resume</Link>
-              <a href="https://github.com/justgarcia5" rel="noopener noreferrer" target="_blank">GitHub</a>
-              {/* <Link to="/aboutme" to="/aboutme">About Me</Link> */}
-              <Link to="/projects2" to="/projects2">Projects</Link>
-              <Link to="/contact" to="/contact">Contact</Link>
-            </Navigation>
-          </Header>
-          <Drawer className="header-links" title={<Link className="header-links" to="/">Justin Garcia</Link>}>
-            <Navigation>
-              <Link to="/resume">Resume</Link>
-              <a href="https://github.com/justgarcia5" rel="noopener noreferrer" target="_blank">GitHub</a>
-              {/* <Link to="/aboutme">About Me</Link> */}
-              <Link to="/projects2">Projects</Link>
-              <Link to="/contact">Contact</Link>
-            </Navigation>
-          </Drawer>
-          <Content>
-            <Routes />
-          </Content>
-        </Layout>
+          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossOrigin="anonymous"></link>
+          <Layout >
+            <Header className="header-links" title={<Link className="justin-header-link" to="/">Justin Garcia</Link>} scroll>
+              <Navigation>
+                <Link style={{fontSize: '20px'}} to="/resume">Resume</Link>
+                <a href="https://github.com/justgarcia5" rel="noopener noreferrer" target="_blank" style={{fontSize: '20px'}}>GitHub</a>
+                {/* <Link to="/aboutme" to="/aboutme">About Me</Link> */}
+                <Link to="/projects2" style={{fontSize: '20px'}} >Projects</Link>
+                <Link to="/contact" style={{fontSize: '20px'}} >Contact</Link>
+              </Navigation>
+            </Header>
+            <Drawer className="header-links" title={<Link className="header-links" to="/">Justin Garcia</Link>}>
+              <Navigation>
+                <Link to="/resume">Resume</Link>
+                <a href="https://github.com/justgarcia5" rel="noopener noreferrer" target="_blank">GitHub</a>
+                {/* <Link to="/aboutme">About Me</Link> */}
+                <Link to="/projects2">Projects</Link>
+                <Link to="/contact">Contact</Link>
+              </Navigation>
+            </Drawer>
+            <Content>
+              <Routes />
+            </Content>
+          </Layout>
         </div>
       </div>
     );
--- src/components/pdfDocs.js
+++ src/components/pdfDocs.js
@@ -1,25 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import Letter from '../pdf_files/letter.pdf'
 import Resume from '../pdf_files/Resume.pdf'
-import CoverLetter from '../pdf_files/CoverLetter.pdf'
 
 const PdfDocs = () => (
   <div className="docs">
-   <div>
-    <h2>Documents</h2>
+    <div>
+      <h2>Documents</h2>
     </div>
     <a href={Letter} rel="noopener noreferrer" target="_blank">
       <i className="fa fa-file-pdf" />
       <p>Letter of Recomendation</p>
     </a>
     <a href={Resume} rel="noopener noreferrer" target="_blank">
-      <i className="fa fa-file-pdf"/>
+      <i className="fa fa-file-pdf" />
       <p>Resume</p>
-    </a>
-    <a href={CoverLetter} rel="noopener noreferrer" target="_blank">
-      <i className="fa fa-file-pdf"  />
-      <p>Cover Letter</p>
     </a>
   </div>
 )
